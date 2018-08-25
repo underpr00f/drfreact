df2049906278bc2ebe18871f5e99b27af63a44f7
underpr00f <underproof2014@gmail.com>
2018-08-25 19:17:55 +0300

    tryin ejeect

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,62 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-// import PostList from './posts/PostList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
-
-import DynamicRouteComp from './routingComps/DynamicRouteComp'
-import StaticRouteComp from './routingComps/StaticRouteComp'
-import NotFound from './routingComps/NotFound'
-
-class DrfReact extends Component {
-  render () {
-    const loggedIn = true
-    const supportsHistory = 'pushState' in window.history
-    return (
-      <div className='App' >
-        <nav className='navbar navbar-light bg-light'>
-          <a className='navbar-brand' href='/'>Navbar</a>
-        </nav>
-        <BrowserRouter forceRefresh={!supportsHistory}>
-          <Switch>
-            <Route exact path='/' component={StaticRouteComp} />
-            <Route exact path='/about' component={StaticRouteComp} />
-            <Route path='/posts/:slug' component={DynamicRouteComp} />
-            <Route component={NotFound} />
-
-            <Route exact path='/user' render={() => (
-              loggedIn === true ? (
-                <Redirect to='/posts/hello-there/' />
-              ) : (
-                <StaticRouteComp />
-              )
-            )} />
-
-          </Switch>
-        </BrowserRouter>
-      </div>
-    )
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-ReactDOM.render(<DrfReact />, document.getElementById('root'));
-// export default AppRoutingExample
-
-
-
-// class DrfReact extends Component {
-// 	render() {
-// 		return (
-// 			<div>
-// 				<PostList />
-// 			</div>
-// 		);
-// 	}
-// }
-
-// ReactDOM.render(<DrfReact />, document.getElementById('root'));
-// registerServiceWorker();
