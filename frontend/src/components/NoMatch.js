a94efbba2492943de05a9e2f6be518713be06e8e
underpr00f <underproof2014@gmail.com>
2019-02-08 00:03:30 +0300

    upgrading npm modules

--- frontend/src/components/NoMatch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoMatch = () => (
-    <h2 className="mt-2">{"Sorry, the page you are looking for doesn't exist."}</h2>
-);
-
-export default NoMatch;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,12 @@
-import "bootstrap/dist/css/bootstrap.css";
-import 'redux-notifications/lib/styles.css';
-import "./styles/style.css"
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import store from "./store";
-import history from "./utils/historyUtils";
-import { authLogin } from "./actions/authActions";
-import App from "./components/App";
-import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-registerServiceWorker();
-
-const token = localStorage.getItem("token");
-
-if (token) {
-    store.dispatch(authLogin(token));
-}
-
-ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <App />
-        </Router>
-    </Provider>
-    , document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
